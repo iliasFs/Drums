45ecef592609daa7d61a7e74ab7f55206b667154
ilias <ilias.fasilakhs@gmail.com>
2022-11-06 10:06:04 +0200

    added removeTransition to reset transition

--- drumkit.js
+++ drumkit.js
@@ -10,7 +10,23 @@ window.addEventListener("keydown", function (event) {
   audio.play(); //when we press the key it plays all the file.In our case it will play for 2 secods.So we need to reset it so when we press the button multiple times, with every click starts again.
   key.classList.add('playing') // we addd the class playing to the buttons 
 
-  //now we want to stop/reset  the animation of the buttons 
-    
    });
+
+//now we want to stop/reset  the animation of the buttons 
+
+const keys = document.querySelectorAll('.key') //pick all keys - console log it
+keys.forEach(key=>{
+    key.addEventListener('transitionend', removeTransition)
+})
+
+function removeTransition(e){
+//console.log(e) to find the e.propertyName = 'transform'
+
+if(e.propertyName !=='transform') 
+return                // skip if its not transform
+console.log(this)
+this.classList.remove('playing')
+
+}
+
 
